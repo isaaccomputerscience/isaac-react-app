2c5449f05481ae461738af9190aa6fa1e960befe
Hannah Whelan <hannah.whelan@and.digital>
2023-08-16 13:29:44 +0100

    removed Teacher CPD, Masterclass and Livestream events - changed event filter to check for booster and discovery

--- src/app/components/site/NavigationBar.tsx
+++ src/app/components/site/NavigationBar.tsx
@@ -49,7 +49,6 @@ export const NavigationBar = () => {
         <NavigationSection title="Events">
             {isTeacherOrAbove(user) && <LinkItem to="/events?show_reservations_only=true">My event reservations</LinkItem>}
             <LinkItem to="/events?types=student">Student events</LinkItem>
-            <LinkItem to="/events?types=teacher">Teacher events</LinkItem>
             <LinkItem to="/pages/event_types">Event formats</LinkItem>
             <LinkItem to="/safeguarding">Safeguarding</LinkItem>
         </NavigationSection>
