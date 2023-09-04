1d1e8bfee5e0b475723f2567884fad9ff47547f4
Hannah Whelan <hannah.whelan@and.digital>
2023-09-04 12:17:31 +0100

    removed ShowLoading from TeacherPromo as it will never be rendered unless promoItem is available

--- src/app/components/elements/TeacherPromoItem.tsx
+++ src/app/components/elements/TeacherPromoItem.tsx
@@ -1,19 +1,13 @@
 import React from "react";
 import { NewsCard } from "./cards/NewsCard";
 import { IsaacPodDTO } from "../../../IsaacApiTypes";
-import { ShowLoading } from "../handlers/ShowLoading";
 
 export const TeacherPromoItem = ({
   item,
 }: {
   item: IsaacPodDTO | undefined;
 }) => (
-  <ShowLoading
-    until={item}
-    thenRender={(newsItem) => (
-      <div className={"my-lg-1 mx-auto p-lg-4 featured-news-item"}>
-        <NewsCard newsItem={newsItem} showTitle linkText={newsItem.subtitle} />
-      </div>
-    )}
-  />
+  <div className={"my-lg-1 mx-auto p-lg-4 featured-news-item"}>
+    {item && <NewsCard newsItem={item} showTitle linkText={item.subtitle} />}
+  </div>
 );
