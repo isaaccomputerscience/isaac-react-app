62826b1d5fee36e7cf9542c471b14f5b87e818eb
Hannah Whelan <hannah.whelan@and.digital>
2023-09-04 14:46:31 +0100

    refactored to use renderTestEnvironment which now allows passing props to component

--- src/test/components/elements/PromoContent.test.tsx
+++ src/test/components/elements/PromoContent.test.tsx
@@ -1,31 +1,19 @@
-import React from "react";
-import { render, screen } from "@testing-library/react";
+import { screen } from "@testing-library/react";
 import { PromoContent } from "../../../app/components/elements/PromoContent";
 import { mockPromoPods } from "../../../mocks/data";
-import { Provider } from "react-redux";
-import { store } from "../../../app/state";
-import { MemoryRouter } from "react-router";
+
 import { IsaacPodDTO } from "../../../IsaacApiTypes";
+import { renderTestEnvironment } from "../../utils";
 
 const mockItem = mockPromoPods.results[0];
 
-const renderPromoContent = ({
-  promoContent,
-}: {
-  promoContent: IsaacPodDTO;
-}) => {
-  render(
-    <Provider store={store}>
-      <MemoryRouter>
-        <PromoContent item={promoContent} />
-      </MemoryRouter>
-    </Provider>
-  );
-};
-
 describe("PromoContent", () => {
   it("renders title, subtitle, image, url, value", () => {
-    renderPromoContent({ promoContent: mockItem });
+    renderTestEnvironment({
+      PageComponent: PromoContent,
+      componentProps: { item: mockItem as IsaacPodDTO },
+      initialRouteEntries: ["/"],
+    });
     const title = screen.getByText(mockItem.title);
     const subtitle = screen.getByText(mockItem.subtitle);
     const imageAltText = screen.getByAltText(mockItem.image.altText);
@@ -43,7 +31,13 @@ describe("PromoContent", () => {
   });
 
   it("if image is not present, default image and alt text are used", () => {
-    renderPromoContent({ promoContent: { ...mockItem, image: undefined } });
+    renderTestEnvironment({
+      PageComponent: PromoContent,
+      componentProps: {
+        item: { ...mockItem, image: undefined } as IsaacPodDTO,
+      },
+      initialRouteEntries: ["/"],
+    });
     const image = screen.getByAltText("promo image");
     expect(image).toBeInTheDocument();
     const imageSrc = image.getAttribute("src");
