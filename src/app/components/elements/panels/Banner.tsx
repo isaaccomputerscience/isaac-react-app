7000080f9dc323ff20aeb8d9c0b18a2e19504053
Hannah Whelan <hannah.whelan@and.digital>
2023-12-20 14:48:11 +0000

    styling changes for alternative banner colours

--- src/app/components/elements/panels/Banner.tsx
+++ src/app/components/elements/panels/Banner.tsx
@@ -7,15 +7,31 @@ export interface BannerProps {
   subtitle: string;
   link: string;
   src: string;
+  secondary?: boolean;
   alt?: string;
 }
 
-export const Banner = ({ id, title, subtitle, link, src, alt, children }: PropsWithChildren<BannerProps>) => {
+export const Banner = ({
+  id,
+  title,
+  subtitle,
+  link,
+  src,
+  secondary,
+  alt,
+  children,
+}: PropsWithChildren<BannerProps>) => {
   return (
-    <section id={id} className="bg-primary pattern-05 p-5">
+    <section
+      id={id}
+      className={`${secondary ? "text-white" : "bg-primary"} pattern-05 p-5`}
+      style={secondary ? { backgroundColor: "#07174f" } : {}}
+    >
       <Container>
         <Row className="d-flex align-items-center justify-content-center">
-          <button className="button-header btn-lg">{title}</button>
+          <button className="btn-lg" style={secondary ? { backgroundColor: "white" } : { backgroundColor: "black" }}>
+            {title}
+          </button>
         </Row>
         <Row className="mt-4 d-flex align-items-center justify-content-center">
           <Col xs={12} md={6} lg={5} xl={4} className="d-flex align-items-center justify-content-center">
@@ -23,7 +39,7 @@ export const Banner = ({ id, title, subtitle, link, src, alt, children }: PropsW
           </Col>
           <Col xs={12} md className="pt-3 pl-3" data-testid="banner-description">
             <h5>
-              <a href={link} target="_blank" rel="noopener noreferrer">
+              <a href={link} target="_blank" rel="noopener noreferrer" className={secondary ? "text-white" : ""}>
                 <strong>{subtitle}</strong>
               </a>
             </h5>
