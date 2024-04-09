cd6d866f8a712c76a7174b34bbdb696381a8093c
Hannah Whelan <hannah.whelan@and.digital>
2024-04-09 11:43:49 +0100

    added image, and redirect to homepage from success if already logged in

--- src/app/components/pages/RegistrationSuccess.tsx
+++ src/app/components/pages/RegistrationSuccess.tsx
@@ -2,8 +2,16 @@ import React from "react";
 import { Card, CardBody, Container } from "reactstrap";
 import { TitleAndBreadcrumb } from "../elements/TitleAndBreadcrumb";
 import { REGISTER_CRUMB } from "../../services";
+import { selectors, useAppSelector } from "../../state";
+import { Redirect } from "react-router";
 
 export const RegistrationSuccess = () => {
+  const user = useAppSelector(selectors.user.orNull);
+
+  if (user?.loggedIn) {
+    return <Redirect to="/" />;
+  }
+
   return (
     <Container id="registration-success" className="mb-5">
       <TitleAndBreadcrumb
@@ -13,6 +21,7 @@ export const RegistrationSuccess = () => {
       />
       <Card>
         <CardBody className="text-center">
+          <img src="/assets/checkemail.svg" alt="check email" className="w-20 mb-4" style={{ maxHeight: "150px" }} />
           <h3>Thank you for providing the required account information.</h3>
           <p> An email has been sent to your provided email address to complete the registration process.</p>
         </CardBody>
