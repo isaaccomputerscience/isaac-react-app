1263c4581ae66eb468f767f2c2fc07494c2d8c19
sxpydo <shavypeynado@gmail.com>
2024-09-03 14:42:25 +0100

    updated h2 header to match the figma design

--- src/app/components/elements/list-groups/ListGroupSocial.tsx
+++ src/app/components/elements/list-groups/ListGroupSocial.tsx
@@ -6,7 +6,7 @@ import { ExternalLink } from "../ExternalLink";
 export const ListGroupSocial = () => {
   return (
     <div className="footer-links footer-links-social">
-      <h2 className="h5">Get social</h2>
+      <h2 className="h5">Social</h2>
       <ListGroup className="mt-3 pb-5 py-lg-3 link-list d-md-flex flex-row">
         {Object.entries(SOCIAL_LINKS).map(([_, { name, href }]) => (
           <ListGroupItem key={name} className="border-0 px-0 py-0 pb-1 bg-transparent">
