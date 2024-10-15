e308d4f5e51d1a83c9d7addafef360f60278ca4b
Parth097 <parthsh197@gmail.com>
2024-10-15 16:14:59 +0100

    Fixed sonar cloud issues

--- src/app/components/site/Header.tsx
+++ src/app/components/site/Header.tsx
@@ -27,27 +27,23 @@ export const Header = () => {
               <div className="header-links ml-auto pr-3 px-md-3 d-flex align-items-center d-print-none">
                 {user &&
                   (!user.loggedIn ? (
-                    <React.Fragment>
-                      <div className="login mx-5 mx-sm-2 d-flex flex-row">
-                        <Link to="/login" className="mr-3">
-                          <span>Log in</span>
-                        </Link>
-                        <Link to="/register">
-                          <span>Sign up</span>
-                        </Link>
-                      </div>
-                    </React.Fragment>
+                    <div className="login mx-5 mx-sm-2 d-flex flex-row">
+                      <Link to="/login" className="mr-3">
+                        <span>Log in</span>
+                      </Link>
+                      <Link to="/register">
+                        <span>Sign up</span>
+                      </Link>
+                    </div>
                   ) : (
-                    <React.Fragment>
-                      <div className="my-account mx-5 mx-sm-2 d-md-flex flex-md-row">
-                        <Link to="/account" className="mr-md-3">
-                          <span>My Account</span>
-                        </Link>
-                        <Link to="/logout">
-                          <span>Logout</span>
-                        </Link>
-                      </div>
-                    </React.Fragment>
+                    <div className="my-account mx-5 mx-sm-2 d-md-flex flex-md-row">
+                      <Link to="/account" className="mr-md-3">
+                        <span>My Account</span>
+                      </Link>
+                      <Link to="/logout">
+                        <span>Logout</span>
+                      </Link>
+                    </div>
                   ))}
               </div>
 
