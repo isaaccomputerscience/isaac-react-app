0a3b8678e203b78e96e0b0f991099ff82628974f
sxpydo <shavypeynado@gmail.com>
2024-11-13 13:43:27 +0000

    added content into the competition page

--- src/app/components/pages/IsaacCompetition.tsx
+++ src/app/components/pages/IsaacCompetition.tsx
@@ -1,7 +1,7 @@
 import React, { useEffect } from "react";
 import { SITE_SUBJECT_TITLE } from "../../services";
 import { TitleAndBreadcrumb } from "../elements/TitleAndBreadcrumb";
-import { Container } from "reactstrap";
+import { Col, Container, Row } from "reactstrap";
 
 export const IsaacCompetition = () => {
   useEffect(() => {
@@ -13,7 +13,32 @@ export const IsaacCompetition = () => {
       <TitleAndBreadcrumb currentPageTitle="Isaac Competition" breadcrumbTitleOverride="Isaac Competition" />
 
       <div id="section1">
-        <h1>Section 1</h1>
+        <Container className="pt-4 z1">
+          <Row>
+            <Col lg={6} xs={12} className="pb-3">
+              <h1 className="homepage-title">National Computer Science Competition 2024/25</h1>
+              <p className="mt-4 homepage-text">
+                Calling all computer science enthusiasts! Isaac Computer Science , ran by the National Centre for
+                Computing Education (NCCE), is hosting a national competition that challenges you to imagine, design,
+                and pitch a groundbreaking new product for the Internet of Everything (IoE). The competition is a
+                fantastic opportunity for students to apply their knowledge to real-world ideas
+              </p>
+              <p className="mt-4 mb-0 homepage-text">
+                Please note: the competition will open in January and last two months. For now, find out more below,
+                follow our X and Facebook account for updates on when you can enter, and teachers can sign up to our
+                expression of interest form.
+              </p>
+            </Col>
+            <Col lg={6} xs={12} className="order-lg-2 order-3 mt-4 mt-lg-0 pb-5 pb-md-0">
+              <img
+                src="/assets/competition-image.png"
+                alt="Competition"
+                className="img-fluid"
+                style={{ maxWidth: "100%", height: "auto" }}
+              />
+            </Col>
+          </Row>
+        </Container>
       </div>
 
       <div id="section2">
