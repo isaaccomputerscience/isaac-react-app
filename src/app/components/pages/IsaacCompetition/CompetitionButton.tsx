060da5ed49c33f9ae8c0013ba23b41d148e9613d
sxpydo <shavypeynado@gmail.com>
2024-11-19 13:43:47 +0000

    made CompetitionButton as a component

--- src/app/components/pages/IsaacCompetition/CompetitionButton.tsx
+++ src/app/components/pages/IsaacCompetition/CompetitionButton.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Col, Row } from "reactstrap";
 
-const CompetitionButton = () => {
-  const competitionInterestButton = [{ to: "/competition-interest", label: "Express your interest" }];
+interface CompetitionButtonProps {
+  buttons: { to: string; label: string }[];
+}
 
-  return (
-    <Row>
-      {competitionInterestButton.map(({ to, label }) => (
-        <Col xs={12} className="py-1" key={to}>
-          <Button size="lg" tag={Link} to={to} block className="primary-button text-light">
-            {label}
-          </Button>
-        </Col>
-      ))}
-    </Row>
-  );
-};
+const CompetitionButton = ({ buttons }: CompetitionButtonProps) => (
+  <Row>
+    {buttons.map(({ to, label }) => (
+      <Col xs={12} className="py-1" key={to}>
+        <Button size="lg" tag={Link} to={to} block className="primary-button text-light">
+          {label}
+        </Button>
+      </Col>
+    ))}
+  </Row>
+);
 
 export default CompetitionButton;
