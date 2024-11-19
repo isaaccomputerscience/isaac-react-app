0bbe2e7051f001cb7f81bc523672b37db352ed06
sxpydo <shavypeynado@gmail.com>
2024-11-19 10:12:36 +0000

    corrected errors

--- src/app/components/pages/IsaacCompetition/CompetitionButton.tsx
+++ src/app/components/pages/IsaacCompetition/CompetitionButton.tsx
@@ -2,20 +2,20 @@ import React from "react";
 import { Link } from "react-router-dom";
 import { Button, Col, Row } from "reactstrap";
 
-export const IsaacCompetition = () => {
-  const CompetitionButton = () => {
-    const competitionInterestButton = [{ to: "/competition-sign-up", label: "Express your interest" }];
+const CompetitionButton = () => {
+  const competitionInterestButton = [{ to: "/competition-interest", label: "Express your interest" }];
 
-    return (
-      <Row>
-        {competitionInterestButton.map(({ to, label }) => (
-          <Col xs={12} className="py-1" key={to}>
-            <Button size="lg" tag={Link} to={to} block className="primary-button text-light">
-              {label}
-            </Button>
-          </Col>
-        ))}
-      </Row>
-    );
-  };
+  return (
+    <Row>
+      {competitionInterestButton.map(({ to, label }) => (
+        <Col xs={12} className="py-1" key={to}>
+          <Button size="lg" tag={Link} to={to} block className="primary-button text-light">
+            {label}
+          </Button>
+        </Col>
+      ))}
+    </Row>
+  );
 };
+
+export default CompetitionButton;
