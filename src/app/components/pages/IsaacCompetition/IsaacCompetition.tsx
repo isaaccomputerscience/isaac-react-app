02065d36884bab7847c06cd438aca8866ce1e056
Parth097 <parthsh197@gmail.com>
2024-11-22 11:38:51 +0000

    basic accordion

--- src/app/components/pages/IsaacCompetition/IsaacCompetition.tsx
+++ src/app/components/pages/IsaacCompetition/IsaacCompetition.tsx
@@ -7,6 +7,7 @@ import "../../../../scss/cs/competition.scss";
 import IoECard from "./Section2/IoECard";
 import TestimonialComment from "../../elements/TestimonialComment";
 import CompetitionButton from "./CompetitionButton";
+import Accordion from "react-bootstrap/Accordion";
 
 const { section1, section2 } = content;
 
@@ -88,6 +89,36 @@ export const IsaacCompetition = () => {
             </div>
           </Container>
         </div>
+      </section>
+      <section id="accordion" className="event-section">
+        <Container>
+          <Row className="py-4">
+            <Col lg={8}>
+              <Accordion defaultActiveKey="0">
+                <Accordion.Item eventKey="0">
+                  <Accordion.Header className="p-3">Accordion Item #1</Accordion.Header>
+                  <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                  </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                  <Accordion.Header>Accordion Item #2</Accordion.Header>
+                  <Accordion.Body>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore
+                    et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut
+                    aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                    cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
+                    culpa qui officia deserunt mollit anim id est laborum.
+                  </Accordion.Body>
+                </Accordion.Item>
+              </Accordion>
+            </Col>
+          </Row>
+        </Container>
       </section>
     </>
   );
