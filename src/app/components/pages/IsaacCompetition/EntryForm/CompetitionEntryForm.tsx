93bd77b67f3e862159dd4c23c7b50e51e2c3fc80
Parth097 <parthsh197@gmail.com>
2024-12-13 16:22:50 +0000

    Pulled in logged in users name, surname and shcool name. Altered the schoolinput file to allow for label to be rendered or not.

--- src/app/components/pages/IsaacCompetition/EntryForm/CompetitionEntryForm.tsx
+++ src/app/components/pages/IsaacCompetition/EntryForm/CompetitionEntryForm.tsx
@@ -2,17 +2,26 @@ import React, { useEffect, useState } from "react";
 import { Form, Row, Col, Button, Container, FormGroup, Label, Input } from "reactstrap";
 import { InputType } from "reactstrap/es/Input";
 import { AppGroup } from "../../../../../IsaacAppTypes";
-import { isaacApi } from "../../../../state";
+import { isaacApi, useAppSelector } from "../../../../state";
+import { selectors } from "../../../../state/selectors";
+import { SchoolInput } from "../../../elements/inputs/SchoolInput";
 
 interface CompetitionEntryFormProps {
   handleTermsClick: (event: React.MouseEvent<HTMLAnchorElement, MouseEvent>) => void;
 }
 
-const renderFormGroup = (label: string, type: string, id: string, options: string[] = []) => (
+const renderFormGroup = (
+  label: string,
+  type: string,
+  id: string,
+  defaultValue: string = "",
+  options: string[] = [],
+  disabled: boolean = false,
+) => (
   <FormGroup>
     <Label className="entry-form-sub-title">{label}</Label>
     {type === "select" ? (
-      <Input type="select" id={id}>
+      <Input type="select" id={id} disabled={disabled}>
         {options?.length > 0 &&
           options.map((option, index) => (
             <option key={`${option}-${index}`} value={option === "Please select from the list" ? "" : option}>
@@ -21,7 +30,7 @@ const renderFormGroup = (label: string, type: string, id: string, options: strin
           ))}
       </Input>
     ) : (
-      <Input type={type as InputType} id={id} />
+      <Input type={type as InputType} id={id} defaultValue={defaultValue} disabled={disabled} />
     )}
   </FormGroup>
 );
@@ -29,6 +38,7 @@ const renderFormGroup = (label: string, type: string, id: string, options: strin
 const CompetitionEntryForm = ({ handleTermsClick }: CompetitionEntryFormProps) => {
   const [activeGroups, setActiveGroups] = useState<AppGroup[]>([]);
   const { data: groups } = isaacApi.endpoints.getGroups.useQuery(false);
+  const targetUser = useAppSelector(selectors.user.orNull);
 
   useEffect(() => {
     if (groups) {
@@ -48,13 +58,38 @@ const CompetitionEntryForm = ({ handleTermsClick }: CompetitionEntryFormProps) =
             <h1 className="py-4 entry-form-title">Enter the competition</h1>
             <Row className="d-flex flex-column flex-md-row">
               <Col lg={6}>
-                {renderFormGroup("Name", "text", "formSubtitle1")}
-                {renderFormGroup("School", "text", "formSubtitle2")}
-                {renderFormGroup("Email", "text", "formSubtitle3")}
+                {renderFormGroup(
+                  "First Name",
+                  "text",
+                  "formSubtitle1",
+                  targetUser?.loggedIn ? targetUser.givenName || "" : "",
+                  [],
+                  true,
+                )}
+                {renderFormGroup(
+                  "Last Name",
+                  "text",
+                  "formSubtitle2",
+                  targetUser?.loggedIn ? targetUser?.familyName || "" : "",
+                  [],
+                  true,
+                )}
+                {targetUser && (
+                  <FormGroup>
+                    <Label className="entry-form-sub-title">School</Label>
+                    <SchoolInput
+                      disableInput={true}
+                      userToUpdate={{ ...targetUser, password: null }}
+                      submissionAttempted={false}
+                      required={true}
+                      showLabel={false}
+                    />
+                  </FormGroup>
+                )}
               </Col>
               <Col lg={6}>
                 {renderFormGroup("Link to submission", "text", "formSubtitle4")}
-                {renderFormGroup("Group", "select", "formSubtitle5", [
+                {renderFormGroup("Group", "select", "formSubtitle5", "", [
                   "Please select from the list",
                   ...activeGroups.map((group) => group.groupName || ""),
                 ])}
