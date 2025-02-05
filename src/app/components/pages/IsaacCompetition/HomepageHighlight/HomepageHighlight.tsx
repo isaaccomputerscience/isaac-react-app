83c45d3e44b5bc6248c7fa0b0b970da4fdf897c8
Parth097 <parthsh197@gmail.com>
2025-02-05 09:59:05 +0000

    changes year to 2025

--- src/app/components/pages/IsaacCompetition/HomepageHighlight/HomepageHighlight.tsx
+++ src/app/components/pages/IsaacCompetition/HomepageHighlight/HomepageHighlight.tsx
@@ -7,7 +7,7 @@ const HomepageHighlight = () => {
     <Container className="pt-2 pb-5">
       <Row className="homepage-highlight rounded justify-content-center">
         <Row className="justify-content-center">
-          <h1 className="homepage-highlight-sub-title py-4">2024 Isaac Computer Science competition</h1>
+          <h1 className="homepage-highlight-sub-title py-4">2025 Isaac Computer Science competition</h1>
           <h1 className="homepage-highlight-title pb-4">Entries are now open</h1>
         </Row>
         <Row className="pb-4">
