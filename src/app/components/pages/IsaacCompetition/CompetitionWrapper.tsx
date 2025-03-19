247fdef6c3066c9373fa143307f3d85fddd57be5
Parth097 <parthsh197@gmail.com>
2025-03-19 18:33:25 +0000

    Added optional children to competition wrapper. Used wrapper to hide away competition features.

--- src/app/components/pages/IsaacCompetition/CompetitionWrapper.tsx
+++ src/app/components/pages/IsaacCompetition/CompetitionWrapper.tsx
@@ -1,13 +1,19 @@
 import React from "react";
 
 const endDate = new Date("2025-03-28T16:00:00"); // 4 PM on Friday, March 28, 2025
+const fourWeeksAfterEndDate = new Date(endDate.getTime() + 4 * 7 * 24 * 60 * 60 * 1000); // 4 weeks after end date
 
 interface CompetitionWrapperProps {
   children: React.ReactNode;
+  afterEndDateChildren?: React.ReactNode;
 }
 
-const CompetitionWrapper = ({ children }: CompetitionWrapperProps) => {
+const CompetitionWrapper = ({ children, afterEndDateChildren }: CompetitionWrapperProps) => {
   const currentDate = new Date();
+
+  if (currentDate > endDate && currentDate <= fourWeeksAfterEndDate) {
+    return <>{afterEndDateChildren}</>;
+  }
 
   if (currentDate > endDate) {
     return null;
