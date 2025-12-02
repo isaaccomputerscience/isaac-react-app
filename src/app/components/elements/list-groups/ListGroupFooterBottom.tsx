a7a10ea2c8f5984bb7f1d0ab751b67e285856f30
Madhura <madhura.bhujbal@gmail.com>
2025-12-02 11:04:12 +0000

    Remove container and fix content position

--- src/app/components/elements/list-groups/ListGroupFooterBottom.tsx
+++ src/app/components/elements/list-groups/ListGroupFooterBottom.tsx
@@ -35,37 +35,34 @@ const footerLinks = {
 };
 
 export const ListGroupFooterBottom = () => (
-  <div className="footer-links footer-bottom">
-    <ListGroup className="d-flex flex-wrap flex-row link-group">
-      <ListGroupItem className="footer-bottom-links d-md-flex flex-md-row bg-transparent link-group w-100">
-        <h2 className="h5 mb-3 mb-md-0 mr-md-4">Links</h2>
-        <ListGroup className="d-md-flex flex-md-row">{footerLinks.right}</ListGroup>
-      </ListGroupItem>
-      <ListGroupItem className="footer-bottom-info border-0 px-0 py-0 bg-transparent">
-        <p className="pl-3 pt-2 mb-lg-0 pb-2 pb-sm-0">
-          All teaching materials on this site are available under the{" "}
-          <ExternalLink
-            href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"
-            className="d-inline fw-bold print-font"
-          >
-            Open Government Licence v3.0
-          </ExternalLink>
-          , except where otherwise stated.
-        </p>
-      </ListGroupItem>
+  <ListGroup className="d-flex flex-wrap flex-row link-group">
+    <ListGroupItem className="footer-bottom-links d-md-flex flex-md-row bg-transparent link-group w-100">
+      <h2>Links</h2>
+      <ListGroup className="d-md-flex flex-md-row">{footerLinks.right}</ListGroup>
+    </ListGroupItem>
+    <ListGroupItem className="footer-bottom-info border-0 px-0 py-0 bg-transparent">
+      <p className="pl-3 pt-2 mb-lg-0 pb-2 pb-sm-0">
+        All teaching materials on this site are available under the{" "}
+        <ExternalLink
+          href="https://www.nationalarchives.gov.uk/doc/open-government-licence/version/3/"
+          className="d-inline fw-bold print-font"
+        >
+          Open Government Licence v3.0
+        </ExternalLink>
+        , except where otherwise stated.
+      </p>
+    </ListGroupItem>
 
-      <ListGroupItem className="footer-bottom-logos border-0 px-0 py-0 bg-transparent d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center d-print-none">
-        <ExternalLink href="https://computingeducation.org.uk/" className="mb-3 mb-md-0">
-          <img
-            src="/assets/logos/ncce.svg"
-            alt="National Centre for Computing Education website"
-            className="img-fluid"
-          />
-        </ExternalLink>
-        <ExternalLink href="https://www.gov.uk/government/organisations/department-for-education">
-          <img src="/assets/logos/dfe.svg" alt="UK Department for Education" className="img-fluid" />
-        </ExternalLink>
-      </ListGroupItem>
-    </ListGroup>
-  </div>
+    <ListGroupItem className="footer-bottom-logos border-0 px-0 py-0 bg-transparent d-flex flex-column flex-md-row justify-content-between align-items-start align-items-md-center d-print-none">
+      <ExternalLink href="https://computingeducation.org.uk/" className="mb-3 mb-md-0">
+        <img src="/assets/logos/ncce.svg" alt="National Centre for Computing Education website" className="img-fluid" />
+      </ExternalLink>
+      <ExternalLink href="https://www.gov.uk/government/organisations/department-for-education">
+        <img src="/assets/logos/dfe.svg" alt="UK Department for Education" className="img-fluid" />
+      </ExternalLink>
+      <ExternalLink href="https://www.stem.org.uk/">
+        <img src="/assets/logos/stem_footer.svg" alt="STEM Learning" className="logo-mr" height="100px" />
+      </ExternalLink>
+    </ListGroupItem>
+  </ListGroup>
 );
