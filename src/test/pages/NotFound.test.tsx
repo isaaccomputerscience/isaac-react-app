08d9cbb520935b7a1c72d67b9a01c38aee132472
Madhura <madhura.bhujbal@gmail.com>
2026-04-02 13:09:45 +0100

    Add tests

--- src/test/pages/NotFound.test.tsx
+++ src/test/pages/NotFound.test.tsx
@@ -3,13 +3,16 @@ import { renderTestEnvironment } from "../utils";
 import { NotFound } from "../../app/components/pages/NotFound";
 
 describe("NotFound page", () => {
-  it("adds a prefilled subject when linking to contact us", () => {
+  it("links to contact form using pageNotFound preset and page id", () => {
     renderTestEnvironment({
       PageComponent: NotFound,
       initialRouteEntries: ["/missing-page"],
     });
 
     const contactLink = screen.getByRole("link", { name: /contact us/i });
-    expect(contactLink).toHaveAttribute("href", '/contact?subject=Page%20not%20found%20%22%2Fmissing-page%22');
+    expect(contactLink).toHaveAttribute(
+      "href",
+      expect.stringContaining("/contact?preset=pageNotFound&page=%2Fmissing-page&url="),
+    );
   });
 });
