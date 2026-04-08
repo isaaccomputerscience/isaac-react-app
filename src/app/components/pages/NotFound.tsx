dac653c8275c3071e86114e696ffe9e7b2dfe055
Madhura <madhura.bhujbal@gmail.com>
2026-04-08 12:20:46 +0100

    Add small font

--- src/app/components/pages/NotFound.tsx
+++ src/app/components/pages/NotFound.tsx
@@ -15,21 +15,19 @@ export const NotFound = () => {
         <TitleAndBreadcrumb breadcrumbTitleOverride="Unknown page" currentPageTitle="Page not found (404)" />
         <h3 className="my-4">
           <small>
-            {"Sorry, we couldn't find the page you were looking for: "}
-            <code>{missingPageId}</code>
-            <br />
-            <br />
-            {"If you entered a web address, check it was correct."}
-            <br />
-            <br />
-            {"If you pasted the web address, check you copied the entire address."}
-            <br />
-            <br />
-            {"If the web address is correct or you selected a link or button, please"}{" "}
-            <a href={contactUrl} style={{ textDecoration: "none", color: "#2B77B4" }}>
-              contact us
-            </a>{" "}
-            {"to let us know."}
+            <p>
+              {"Sorry, we couldn't find the page you were looking for: "}
+              <code>{missingPageId}</code>
+            </p>
+            <p>{"If you entered a web address, check it was correct."}</p>
+            <p>{"If you pasted the web address, check you copied the entire address."}</p>
+            <p>
+              {"If the web address is correct or you selected a link or button, please"}{" "}
+              <a href={contactUrl} style={{ textDecoration: "none", color: "#2B77B4" }}>
+                contact us
+              </a>{" "}
+              {"to let us know."}
+            </p>
           </small>
         </h3>
       </div>
