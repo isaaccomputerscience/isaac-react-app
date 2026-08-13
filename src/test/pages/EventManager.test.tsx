cd7af6d95029c9efd62db2988227e5407e1ff1ea
Madhura <madhura.bhujbal@gmail.com>
2026-08-13 17:12:18 +0100

    Remove the tests for the event buttons

--- src/test/pages/EventManager.test.tsx
+++ src/test/pages/EventManager.test.tsx
@@ -33,17 +33,6 @@ describe("Event Manager", () => {
     );
   });
 
-  it("shows event listing form and events toolkit buttons with correct links", () => {
-    renderEventManager();
-    const eventListingForm = screen.getByRole("link", { name: /event listing form/i });
-    const eventsToolkit = screen.getByRole("link", { name: /events toolkit/i });
-    expect(eventListingForm).toHaveAttribute("href", "https://forms.office.com/e/ZrijWx8gcw");
-    expect(eventsToolkit).toHaveAttribute(
-      "href",
-      "https://myscience.atlassian.net/wiki/spaces/NN/pages/4119658517/Events+toolkit",
-    );
-  });
-
   it("if logged in as an Event Leader, a message appears to warn that they can only view events they manage", () => {
     renderEventManager({ role: "EVENT_LEADER" });
     checkPageTitle("Event booking admin");
